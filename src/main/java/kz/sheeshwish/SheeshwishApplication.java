24702f6519c493744796b1e9aabe697ad88f1a5e
fufn <xfufnx@gmail.com>
2025-01-28 01:43:18 +0500

    initialization

---
+++ src/main/java/kz/sheeshwish/SheeshwishApplication.java
@@ -0,0 +1,13 @@
+package kz.sheeshwish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheeshwishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheeshwishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/sheeshwish/SheeshwishApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sheeshwish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheeshwishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
