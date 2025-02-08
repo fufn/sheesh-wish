1e7bb2eaf41b2664445b0165401d3704dcd574c7
fufn <xfufnx@gmail.com>
2025-02-08 18:20:08 +0500

    changed entity

--- src/main/java/kz/wishify/dto/WishDto.java
+++ src/main/java/kz/wishify/dto/WishDto.java
@@ -1,11 +1,17 @@
 package kz.wishify.dto;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public record WishDto(
         UUID id,
         String title,
         String description,
-        String username
-) {
-}
+        Boolean isPurchased,
+        String username,
+        Double price,
+        Instant targetDate,
+        String imageUrl,
+        String productUrl,
+        Instant createdAt
+) {}
